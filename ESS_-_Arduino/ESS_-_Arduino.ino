9e98c354e4869912b7373cccd2a1ec70a501e884
Eudson Souza <emas@cin.ufpe.br>
2018-06-18 14:46:12 -0300

    Adicionar led de sinalização

--- ESS_-_Arduino/ESS_-_Arduino.ino
+++ ESS_-_Arduino/ESS_-_Arduino.ino
@@ -1,19 +1,25 @@
 #include <SoftwareSerial.h> 
 #include <TinyGPS.h>
 #define GPS_DELAY_MS 1500
-#define BUTTON_PIN 7
-
+#define BUTTON_PIN 8
+#define RGB_GREEN_PIN 10
+#define RGB_RED_PIN 11
+#define RGB_BLUE_PIN 9
 float lat = 1.000,lon = 1.000000; // create variable for latitude and longitude object
 bool activateAlarm = false;  
 SoftwareSerial gpsSerial(4,3);//rx,tx 
 TinyGPS gps; // create gps object 
+
 
 void setup(){ 
 Serial.begin(9600); // connect serial 
 Serial.println("The GPS Received Signal:"); 
 gpsSerial.begin(9600); // connect gps sensor
 pinMode(BUTTON_PIN,INPUT);
-
+pinMode(RGB_GREEN_PIN,OUTPUT);
+pinMode(RGB_RED_PIN,OUTPUT);
+pinMode(RGB_BLUE_PIN,OUTPUT);
+unsigned long start_teste = millis();
 }
 
 void getPosition(){ // get latitude and longitude 
@@ -39,13 +45,21 @@ void getPosition(){ // get latitude and longitude
 
 
 void loop(){
-  if(digitalRead(BUTTON_PIN) == 1){
+  
+  if(digitalRead(BUTTON_PIN)){
     activateAlarm = true;
   }
   if(activateAlarm){
    getPosition();
+   analogWrite(RGB_GREEN_PIN,0);
+   analogWrite(RGB_RED_PIN, 50);
+   
   }
-  Serial.println(digitalRead(BUTTON_PIN));
+  else{
+   analogWrite(RGB_GREEN_PIN,50);
+   analogWrite(RGB_RED_PIN, 0);
+  }
+  
  
 } 
 
