1b5d496d9a649c4d836321f8a8b92988cada585a
Eudson Souza <emas@cin.ufpe.br>
2018-06-18 11:30:53 -0300

    Leitura de dados do GPS a cada 1 seg

---
+++ ESS_-_Arduino/ESS_-_Arduino.ino
@@ -0,0 +1,59 @@
+#include <SoftwareSerial.h> 
+#include <TinyGPS.h>
+#define GPS_DELAY_MS 1500
+float lat = 1.000,lon = 1.000000; // create variable for latitude and longitude object  
+SoftwareSerial gpsSerial(4,3);//rx,tx 
+TinyGPS gps; // create gps object 
+
+void setup(){ 
+Serial.begin(9600); // connect serial 
+Serial.println("The GPS Received Signal:"); 
+gpsSerial.begin(9600); // connect gps sensor 
+
+}
+/*static void smartdelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (ss.available())
+      gps.encode(ss.read());
+  } while (millis() - start < ms);
+}*/
+void getPosition(){ // get latitude and longitude 
+   static unsigned long start = millis();
+   if(millis() - start > GPS_DELAY_MS){
+      while(gpsSerial.available()){ // check for gps data 
+          if(gps.encode(gpsSerial.read())){// encode gps data   
+            gps.f_get_position(&lat,&lon); // get latitude and longitude 
+            // display position 
+            Serial.println("GPS Signal");
+            Serial.print("Position: "); 
+            Serial.print("Latitude:"); 
+            Serial.print(lat,6); 
+            Serial.print(";"); 
+            Serial.print("Longitude:"); 
+            Serial.println(lon,6);  
+            Serial.println(millis() - start);
+            start = millis();
+          }
+           
+      }
+    }      
+}
+
+
+void loop(){
+  static unsigned long start_test = millis();
+
+  
+    Serial.println("rodando main");
+  
+  
+  getPosition();
+  
+   
+  
+} 
+
+ 
