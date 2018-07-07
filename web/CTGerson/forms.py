4745b9f116d5f8aad4666896afe391000e1315fe
Marcello Cordeiro <msc3@cin.ufpe.br>
2018-07-07 19:09:14 -0300

    Minor refactoring

--- web/CTGerson/forms.py
+++ web/CTGerson/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Bus
 
+
 class BusForm(forms.ModelForm):
     class Meta:
         model = Bus
--- web/CTGerson/models.py
+++ web/CTGerson/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Bus(models.Model):
-    plate = models.CharField(max_length = 8)
-    line_number = models.CharField(max_length = 5)
-    company = models.CharField(max_length = 100)
+    plate = models.CharField(max_length=8)
+    line_number = models.CharField(max_length=5)
+    company = models.CharField(max_length=100)
 
     def __str__(self):
         return self.plate
 
-#class Ocurrence(models.Model):
+
+# class Ocurrence(models.Model):
 #    busid = models.ForeignKey('CTGerson.Bus')
