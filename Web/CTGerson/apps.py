713d864b129551ffe153f278f8bf4ca6542c890c
Marcello Cordeiro <msc3@cin.ufpe.br>
2018-07-07 17:55:57 -0300

    Update gitignore

--- Web/CTGerson/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CtgersonConfig(AppConfig):
-    name = 'CTGerson'
--- Web/CTGerson/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Bus
-
-class BusForm(forms.ModelForm):
-    class Meta:
-        model = Bus
-        fields = ('plate', 'line_number', 'company')
