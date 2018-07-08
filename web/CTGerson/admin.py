2498c42dfc45258edbfd72b2e18ce36eea851ffe
Unknown <ccal2@cin.ufpe.br>
2018-07-07 21:43:54 -0300

    Occurence model

--- web/CTGerson/admin.py
+++ web/CTGerson/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Bus
+from .models import Bus, Ocurrence
 
 # Register your models here.
 admin.site.register(Bus)
+admin.site.register(Ocurrence)
