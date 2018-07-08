e3d1b4c4f1822cab6b4aed0c10bf3d541cd47a24
Unknown <ccal2@cin.ufpe.br>
2018-07-07 23:29:54 -0300

    Login verification update

--- web/CTGerson/views.py
+++ web/CTGerson/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Bus, Occurrence
 from .forms import BusForm
 
 
+@login_required
 def home(request):
-    if request.user.is_authenticated:
-        return render(request, 'home.html', {})
-    else:
-        return redirect('login')
+    return render(request, 'home.html', {})
 
 
+@login_required
 def bus_list(request):
     buses = Bus.objects.all()
     return render(request, 'bus_list.html', {'buses': buses})
 
 
+@login_required
 def register_bus(request):
     if request.method == "POST":
         form = BusForm(request.POST)
@@ -27,6 +28,7 @@ def register_bus(request):
     return render(request, 'register_bus.html', {'form': form})
 
 
+@login_required
 def edit_bus(request, pk):
     bus = get_object_or_404(Bus, pk=pk)
 
@@ -42,18 +44,23 @@ def edit_bus(request, pk):
     return render(request, 'edit_bus.html', {'form': form})
 
 
+@login_required
 def remove_bus(request, pk):
     bus = Bus.objects.get(id=pk)
     bus.delete()
 
     return redirect('bus_list')
 
+
+@login_required
 def bus_detail(request, pk):
     bus = get_object_or_404(Bus, pk=pk)
     occurrences = Occurrence.objects.filter(bus=bus)
 
     return render(request, 'bus_detail.html', {'bus': bus, 'occurrences': occurrences})
 
+
+@login_required
 def occurrences_list(request):
     occurrences = Occurrence.objects.all()
     return render(request, 'occurrences_list.html', {'occurrences': occurrences})
