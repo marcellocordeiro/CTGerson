5157965bcb1c5323a6bff6b04c1deaafce800162
Marcello Cordeiro <msc3@cin.ufpe.br>
2018-07-09 18:17:59 -0300

    Update README and refactor Arduino source file

--- README.md
+++ README.md
@@ -11,3 +11,40 @@
 @vvs17
 
 CTGerson is a bus-security system concept made for the Systems and Software Engineering discipline of the Federal University of Pernambuco.
+
+## Instructions
+
+```[delete this] Arduino instructions and setup```
+
+1. Download the repo
+
+```console
+git clone https://github.com/marcellocordeiro/CTGerson
+```
+
+2. Create and enter a virtual environment
+
+```console
+mkdir myenv
+python3 -m venv myenv
+source myenv/bin/activate
+```
+
+3. Update pip and install Django
+
+```console
+python3 -m pip install --upgrade pip
+pip3 install Django
+```
+
+4. Run the web server
+
+```console
+python3 web/manage.py runserver
+```
+
+Cleaning the project's folder
+
+```console
+find . | grep -E "(.DS_Store|__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
+```
--- src/arduino.ino
+++ src/arduino.ino
@@ -1,44 +1,106 @@
+#include <KNoTThing.h>
 #include <SoftwareSerial.h>
 #include <TinyGPS.h>
 
 #define GPS_DELAY_MS 1500
-#define BUTTON_PIN 8
+#define BUTTON_PIN 3
 #define RGB_GREEN_PIN 10
 #define RGB_RED_PIN 11
 #define RGB_BLUE_PIN 9
 
-float lat = 1.000, lon = 1.000000;  // create variable for latitude and longitude object
-bool activateAlarm = false;
+#define LON_ID 2
+#define LON_NAME "Longitude"
 
-SoftwareSerial gpsSerial(4, 3);  // rx,tx
+#define LAT_ID 3
+#define LAT_NAME "Latitude"
+
+#define ALARM_STATUS_ID 4
+#define ALARM_STATUS_NAME "Alarm Status"
+
+float lat = 2.123123, lon = 3.312312;  // create variable for latitude and longitude object
+bool statusAlarm = false;
+
+SoftwareSerial gpsSerial(5, 4);  // rx,tx
 TinyGPS gps;                     // create gps object
+KNoTThing thing;
+
+static float lon_read(int32_t *val_int, uint32_t *val_dec, int32_t *multiplier) {
+    *val_int = (int)lon;
+    *val_dec = (lon - *val_int) * 1000000;
+    *multiplier = 1;
+    return 0;
+}
+static float lon_write(int32_t *val_int, uint32_t *val_dec, int32_t *multiplier) {
+    return 0;
+}
+
+static float lat_read(int32_t *val_int, uint32_t *val_dec, int32_t *multiplier) {
+    *val_int = (int)lat;
+    *val_dec = (lat - *val_int) * 1000000;
+    *multiplier = 1;
+
+    return 0;
+}
+
+static float lat_write(int32_t *val_int, uint32_t *val_dec, int32_t *multiplier) {
+    return 0;
+}
+
+static int alarm_status_read(uint8_t *val) {
+    *val = statusAlarm;
+    return 0;
+}
+
+static int alarm_status_write(uint8_t *val) {
+    statusAlarm = *val;
+
+    return 0;
+}
 
 void setup() {
     Serial.begin(9600);  // connect serial
-    Serial.println("The GPS Received Signal:");
+
+    // Serial.println("The GPS Received Signal:");
     gpsSerial.begin(9600);  // connect gps sensor
+
     pinMode(BUTTON_PIN, INPUT);
     pinMode(RGB_GREEN_PIN, OUTPUT);
     pinMode(RGB_RED_PIN, OUTPUT);
     pinMode(RGB_BLUE_PIN, OUTPUT);
+
     unsigned long start_teste = millis();
+
+    // KNoT code
+    thing.init("ALARME DAORA");
+    thing.registerBoolData(ALARM_STATUS_NAME, ALARM_STATUS_ID, KNOT_TYPE_ID_SWITCH, KNOT_UNIT_NOT_APPLICABLE, alarm_status_read, alarm_status_write);
+
+    // thing.registerFloatData(LON_NAME, LON_ID, KNOT_TYPE_ID_LONGITUDE,
+    //   KNOT_UNIT_LONGITUDE_DEGREE, lon_read,lon_write);
+
+    thing.registerFloatData(LAT_NAME, LAT_ID, KNOT_TYPE_ID_LATITUDE, KNOT_UNIT_LATITUDE_DEGREE, lat_read, lat_write);
+
+    /* Send data every 10 seconds*/
+    // thing.registerDefaultConfig(LON_ID, KNOT_EVT_FLAG_TIME, 1, 0, 0, 0, 0);
+    thing.registerDefaultConfig(LAT_ID, KNOT_EVT_FLAG_TIME, 1, 0, 0, 0, 0);
+    thing.registerDefaultConfig(ALARM_STATUS_ID, KNOT_EVT_FLAG_TIME, 1, 0, 0, 0, 0);
 }
 
 void getPosition() {  // get latitude and longitude
     static unsigned long start = millis();
+
     if (millis() - start > GPS_DELAY_MS) {
         while (gpsSerial.available()) {          // check for gps data
             if (gps.encode(gpsSerial.read())) {  // encode gps data
                 gps.f_get_position(&lat, &lon);  // get latitude and longitude
                 // display position
-                Serial.println("GPS Signal");
-                Serial.print("Position: ");
-                Serial.print("Latitude:");
-                Serial.print(lat, 6);
-                Serial.print(";");
-                Serial.print("Longitude:");
-                Serial.println(lon, 6);
-                Serial.println(millis() - start);
+                // Serial.println("GPS Signal");
+                // Serial.print("Position: ");
+                // Serial.print("Latitude:");
+                // Serial.print(lat,6);
+                // Serial.print(";");
+                // Serial.print("Longitude:");
+                // Serial.println(lon,6);
+                // Serial.println(millis() - start);
                 start = millis();
             }
         }
@@ -46,16 +108,7 @@ void getPosition() {  // get latitude and longitude
 }
 
 void loop() {
-    if (digitalRead(BUTTON_PIN)) {
-        activateAlarm = true;
-    }
-
-    if (activateAlarm) {
-        getPosition();
-        analogWrite(RGB_GREEN_PIN, 0);
-        analogWrite(RGB_RED_PIN, 50);
-    } else {
-        analogWrite(RGB_GREEN_PIN, 50);
-        analogWrite(RGB_RED_PIN, 0);
-    }
+    thing.run();
+    getPosition();
+    statusAlarm = digitalRead(BUTTON_PIN);
 }
